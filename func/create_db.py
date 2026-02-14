bfa24eda18d2ad3a488b1a2e73a8a06eb8dd7cd0
MrYaz <LehaHotPlay@yandex.ru>
2026-02-15 01:52:54 +0500

    созданна функция, которая создаёт базу данных, если её нет.
    также добавленна функция, создающая пользователя в базе

--- func/create_db.py
+++ func/create_db.py
@@ -7,6 +7,8 @@
 # 5. Количесвто дней сколько он уже занимается
 # 6. Номер дня / сколько правильних и неправильных ответов
 
+# Чтоб создать дб, открыть это файл
+
 from pathlib import Path
 import sqlite3
 
@@ -15,8 +17,32 @@ path = script_dir / 'func' / 'date' / 'users.db'
 
 # Подключение (файл создастся, если его нет)
 conn = sqlite3.connect(path)
+
 cursor = conn.cursor()
-# Создание таблицы
-cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)')
-conn.commit()
-conn.close()
+
+def create_user(id: str, count_m: int, start: str, stop: str):
+    cursor.execute(f'''
+INSERT OR REPLACE INTO users (id, count_messages_in_day, start_message, stop_message)
+VALUES (?, ?, ?, ?  )
+''', (id, count_m, start, stop))
+    conn.commit()
+
+    cursor.execute("SELECT * FROM users")
+    print(cursor.fetchall())
+
+if __name__ == '__main__':
+
+
+    # Создание таблицы
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY, 
+        count_messages_in_day SMALLINT, 
+        start_message VARCHAR,
+        stop_message VARCHAR,
+        count_work TINYINT
+    );   
+    ''')
+
+
+    conn.commit()
+    conn.close()
