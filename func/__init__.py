ff837de5c4d81fdf38aeec5fb6bbeae56316d1e2
MrYaz <LehaHotPlay@yandex.ru>
2026-03-04 11:07:27 +0500

    Добавлен файл, отвечающий за подключение всех внешних команд (__init__.py):
    Теперь из любой точки кода можно подключить func

---
+++ func/__init__.py
@@ -0,0 +1,13 @@
+from .data_base.create_db import create_user, get_all_users, get_winstrik, add_winstrik, clear_winstrik, set_visible_concurs, ban_user, set_null_quest, set_a_b_quest, get_user
+from .generate_and_answer import generate_math
+
+# Routers
+from .command_start import router as startRouter
+
+_all__ = ['create_user', 'get_all_users', 'get_winstrik', 'add_winstrik', 'clear_winstrik', 'set_visible_concurs', 'ban_user', 'set_null_quest', 'set_a_b_quest', 'get_user', # data_base_work
+          'generate_math', # math_work
+          
+          
+          'startRouter' # Routers
+          ]
+
